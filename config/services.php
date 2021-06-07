fe5fa14fa85490931d908edf150019f2e6730d72
jervills29 <82385160+jervills29@users.noreply.github.com>
2021-06-07 22:46:06 +0800

    first commit

---
+++ config/services.php
@@ -0,0 +1,13 @@
+<?php
+
+return[
+    'user1' =>[
+        'base_uri'=>env('USER1_SERVICE_BASE_URL'),
+        'secret'=>env('USER1_SERVICE_SECRET'),
+    ],
+    'user2' =>[
+        'base_uri'=>env('USER2_SERVICE_BASE_URL'),
+        'secret'=>env('USER2_SERVICE_SECRET'),
+    ]
+
+];
